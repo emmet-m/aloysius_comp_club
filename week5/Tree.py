8e0a40c783a34db429477ffdd192df6aac49f29f
Emmet Murray <e.minor.murray@gmail.com>
2017-06-09 20:32:26 +1000

    Shuffled things into appropriate files, added 2 more test files

--- week5/Tree.py
+++ week5/Tree.py
@@ -5,3 +5,30 @@ class Tree(object):
 
 	def getChildren():
 		return this.children
+
+def parseTreeInput(fileName):
+	f = open(fileName, 'r')
+	lines = f.readlines()
+	info = {}
+	for line in lines:
+		line = line.rstrip()
+		parent = Tree(line.split(":")[0])
+		children = line.split(":")[1].split(" ")
+		dat = [parent] + children
+		info[line.split(":")[0]] = dat
+
+	# Done
+	addChildren('1', info)
+	
+	# return the root
+	return info["1"][0]
+
+def addChildren(nodeId, info):
+	if nodeId == '':
+		return
+	dat = info[nodeId]
+	node = dat[0]
+	children = dat[1:]
+	for child in children:
+		node.children = node.children + [addChildren(child, info)]
+	return node
--- week5/search.py
+++ week5/search.py
@@ -1,47 +1,14 @@
 #!/usr/bin/env python3.5
-import re
-from Tree import Tree
 
-#
-# Ignore these next two functions :^) We'll go over them in class.
-#
-def parseTreeInput(fileName):
-	f = open(fileName, 'r')
-	lines = f.readlines()
-	info = {}
-	for line in lines:
-		line = line.rstrip()
-		parent = Tree(line.split(":")[0])
-		children = line.split(":")[1].split(" ")
-		dat = [parent] + children
-		info[line.split(":")[0]] = dat
-
-	# Done
-	addChildren('1', info)
-	
-	# return the root
-	return info["1"][0]
-
-def addChildren(nodeId, info):
-	if nodeId == '':
-		return
-	dat = info[nodeId]
-	node = dat[0]
-	children = dat[1:]
-	for child in children:
-		node.children = node.children + [addChildren(child, info)]
-	return node
-#
-# Everything that interests you is below here!
-#
+# Include our tree library
+from Tree import *
 
 # This is your search function to fill in
-# You can get info on stacks here: https://docs.python.org/3.5/tutorial/datastructures.html#using-lists-as-stacks
 
-def depthFirstSearch(tree, key):
+def dfs(tree, key):
 	#Get rid of this line to execute your function
 	pass
 
 #
 tree = parseTreeInput("tree1.txt")
-# Test your searching below
+# Test your searching with some examples below!
